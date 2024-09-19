94b0f1e57a4ac6fac51767bed5e896b4f8878886
mariam-khasawneh <khasawnehmariam@gmail.com>
2024-09-19 10:13:36 +0300

    create doctor profile page interface

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Import your pages/components
-import Home from './pages/home';
-
+import Home from "./pages/home";
+// import DoctorProfilePage from "./pages/DoctorProfilePage";
+import DoctorProfilePage from "./pages/Doctor Profile/DoctorProfilePage";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />       
+        <Route path="/" element={<Home />} />
+        <Route path="/doctor-profile" element={<DoctorProfilePage />} />
       </Routes>
     </BrowserRouter>
   );
