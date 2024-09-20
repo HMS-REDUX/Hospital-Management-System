6064d7604b534fc0187b1666db28e717de31b1de
Abedelrauof <abedelrauofyousef@gmail.com>
2024-09-20 15:36:56 +0300

    desings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />       
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
