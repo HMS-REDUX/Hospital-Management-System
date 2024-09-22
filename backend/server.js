8c220dfb18d595fd1dea1df387b8124b82e6af9d
yousefborini02 <yousefborinii@gmail.com>
2024-09-22 15:50:33 +0300

    yousef branch

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,17 @@ const app = express();
 const PORT = process.env.PORT;
 const authroutes = require("./routes/authRoutes");
 
+
+
+/////////////////////////////////Admin////////////////////////////////////////////////
+const userRoutesAdmin = require("./routes/userRoutesAdmin"); // Add this line
+const doctorRoutesAdmin = require("./routes/doctorRoutesAdmin"); // Import the new route
+const appointmentRoutesAdmin = require("./routes/appointmentRoutesAdmin"); // Import appointment routes
+const contactRoutesAdmin = require("./routes/contactRoutesAdmin"); // Add this line
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+
 // CORS configuration
 const corsOptions = {
   origin: "http://localhost:5173", // Replace with the URL of your frontend
@@ -16,6 +27,18 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api/auth", authroutes);
+
+
+
+/////////////////////////////////Admin////////////////////////////////////////////////
+app.use("/api", userRoutesAdmin); // Add the user routes here
+app.use("/api", doctorRoutesAdmin); // Use the new route here
+app.use("/api", appointmentRoutesAdmin); // Use the appointment routes
+app.use("/api", contactRoutesAdmin); // Add the contact routes here
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,15 @@ import Register from "./pages/Register";
 import Home from "./pages/home";
 import DoctorProfilePage from "./pages/Doctor Profile/DoctorProfilePage";
 
+///////////////////////Admin///////////////////////////////////
+import HomePage from "./pages/AdminDashboard/HomePage";
+import PatientRecords from './pages/AdminDashboard/PatientRecords';
+import Doctors from "./pages/AdminDashboard/Doctors";
+import Stats from './pages/AdminDashboard/Stats';
+import Appointments from "./pages/AdminDashboard/Appointments";
+import Messages from "./pages/AdminDashboard/Messages";
+///////////////////////////////////////////////////////////////
+
 function App() {
   return (
     <Routes>
@@ -13,6 +22,26 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+
+
+
+
+                       {/* Admin Dashbord */}
+
+      <Route path="/admin" element={<HomePage />}>
+          <Route index element={<Stats />} />
+          <Route path="patient-records" element={<PatientRecords />} />
+          <Route path="doctors" element={<Doctors />} />
+          <Route path="appointments" element={<Appointments />} />
+          <Route path="messages" element={<Messages/>} />
+      </Route>
+
+                      {/* Admin Dashbord */}
+
+
+
+
+
     </Routes>
   );
 }
